93079033a2e507129b6a5997bd6b319c71f126b4
AdityacodeNIT <srivastavaditya145@gmail.com>
2024-10-13 18:54:02 +0530

    indexpush

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,21 @@
 import mongoose from "mongoose";
 
-import { DB_NAME } from "../constants.js";
-
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
-    );
-
-    console.log(
-      `\n mongo db connected db host : ${connectionInstance.connection.host} db_name: ${DB_NAME}`,
-    );
-  } catch (error) {
-    console.error("MONGODB CONNECTION ERROR", error);
-    process.exit(1);
-  }
+        try {
+                const connectionInstance = await mongoose.connect(
+                        process.env.MONGODB_URI,
+                        {
+                                useNewUrlParser: true,
+                                useUnifiedTopology: true,
+                        },
+                );
+                console.log(
+                        `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
+                );
+        } catch (error) {
+                console.log("MONGODB connection FAILED ", error);
+                process.exit(1);
+        }
 };
+
 export default connectDB;
