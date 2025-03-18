41477dda4b6c20328a70a4179189f588e017ea32
AdityacodeNIT <srivastavaditya145@gmail.com>
2025-03-18 22:48:33 +0530

    Add user interaction tracking and recommendations; integrate Python model for training and fetching recommendations

--- src/routes/interaction.routes.js
+++ src/routes/interaction.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { saveUserInteraction } from "../controllers/Interaction.controllers.js";
+import { saveUserInteraction,getUserRecommendations } from "../controllers/Interaction.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const interactionRouter=Router();
 
 interactionRouter.route("/record").post(verifyJWT,saveUserInteraction);
+
+interactionRouter.route("/recommendations").get(verifyJWT,getUserRecommendations);
 
 
 export default interactionRouter;
